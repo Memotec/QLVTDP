ed396ac636e5d72ab571d26b11bf6a37f910df42
Memotec <41383155+Memotec@users.noreply.github.com>
2026-08-26 16:12:21 +0700

    Update company name and department in header

--- src/App.tsx
+++ src/App.tsx
@@ -1169,8 +1169,8 @@ export default function App() {
           <table class="header-table">
             <tr>
               <td class="header-left">
-                <div style="text-transform: uppercase;">TỔNG CÔNG TY QUẢN LÝ BAY VIỆT NAM</div>
-                <div style="text-transform: uppercase; font-weight: 900; margin-top: 3px;">ĐỘI THÔNG TIN HÀNG KHÔNG</div>
+                <div style="text-transform: uppercase;">CÔNG TY QUẢN LÝ BAY MIỀN NAM</div>
+                <div style="text-transform: uppercase; font-weight: 900; margin-top: 3px;">TRUNG TÂM BĐKT-ĐỘI THÔNG TIN</div>
                 <div style="font-weight: normal; font-style: italic; margin-top: 2px;">Bộ phận: Quản lý Kho CNS/ATM</div>
                 <div class="deco-line"></div>
               </td>
